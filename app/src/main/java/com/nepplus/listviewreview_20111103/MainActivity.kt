5c652cfd3f6016f8e76e6a9899fe13671a17a67f
이아현 <suk03029@gmail.com>
2021-11-03 15:41:12 +0900

    startReview

---
+++ app/src/main/java/com/nepplus/listviewreview_20111103/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.nepplus.listviewreview_20111103
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
